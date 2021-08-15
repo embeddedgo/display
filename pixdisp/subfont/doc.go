807d9b8d929e2a6a0fe28d10f39e3793ac9088e4
Michał Derkacz <michal@Lnet.pl>
2021-08-16 00:50:55 +0200

    pixdisp/subfont: completly rewrite font9

--- pixdisp/subfont/doc.go
+++ pixdisp/subfont/doc.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package subfont describes a font as a collection of subfonts. A Subfont holds
-// the character data for a specific size, style, weight and specific range of
-// unicode codepoints.
+// Package subfont assumes that a font is a collection of subfonts. A Subfont
+// holds the character data for a specific size, style, weight and specific
+// range of unicode codepoints.
 package subfont
--- pixdisp/subfont/font9/format.go
+++ pixdisp/subfont/font9/format.go
@@ -4,30 +4,56 @@
 
 package font9
 
-// Data implements subfont.Data interface storing characters as a single
-// image with the glyphs placed side-by-side on a common baseline.
-type Data struct {
-	Info Info  // character descriptions
-	Bits Image // image holding the glyphs, the baseline is at y = 0
+import "image"
+
+// Mono can hold data of a monospace font.
+type Mono struct {
+	Left  int8  // offset to the origin
+	Adv   uint8 // distance between two successive glyph origins when drawing
+	Width uint8 // width of the glyph subimage
+	Bits  Image // image holding the glyphs, the baseline is at y = 0
 }
 
-// Info is the interface that wraps the Fontchar method.
-//
-// Fontchar descibes i-th character glyph in the subfont. The difference to the
-// original Plan 9 fontchar is the lack of the top and bottom information.
-type Info interface {
-	Fontchar(i int) (x, left, advance int)
+// Advance implements subfont.Data interface
+func (d *Mono) Advance(i int) int {
+	return int(d.Width)
 }
 
-// Mono implements Info interface for monospace font.
-type Mono uint8
+// Glyph implements subfont.Data interface
+func (d *Mono) Glyph(i int) (img image.Image, origin image.Point, advance int) {
+	r := d.Bits.Bounds()
+	r.Min.X += int(d.Width) * i
+	r.Max.X = r.Min.X + int(d.Width)
+	img = d.Bits.SubImage(r)
+	origin = image.Point{r.Min.X + int(d.Left), 0}
+	advance = int(d.Adv)
+	return
+}
 
-// Prop implements Info interface for proportional font. Each character is
-// described using 4 bytes (xh, xl, left, advance) where the x = uint16(xh)<<8 +
-// uint16(xl) is the glyph position in the image, int8(left) is the offset
-// to the glyph origin, uint8(advance) is the distance between two successive
-// glyph origins when drawing.
-type Prop []byte
+// Prop can hold data of a proportional font. The difference to the original
+// Plan 9 subfont is the lack of the top and bottom information in Info. Info is
+// stored in a string to avoid copying it from Flash to RAM in case of embedded
+// systems. Use strings.Builder to efficiently load/build it at runtime.
+type Prop struct {
+	Info string // character descriptions N x 4 bytes (advance, left, xl, xh)
+	Bits Image  // image holding the glyphs, the baseline is at y = 0
+}
 
-// ImmProp is immutable version of Prop.
-type ImmProp string
+// Advance implements subfont.Data interface
+func (d *Prop) Advance(i int) int {
+	return int(d.Info[i*4])
+}
+
+// Glyph implements subfont.Data interface
+func (d *Prop) Glyph(i int) (img image.Image, origin image.Point, advance int) {
+	r := d.Bits.Bounds()
+	info := d.Info[i*4:]
+	if len(info) >= 8 {
+		r.Max.X = r.Min.X + (int(info[6]) | int(info[7])<<8)
+	}
+	r.Min.X += int(info[2]) | int(info[3])<<8
+	img = d.Bits.SubImage(r)
+	origin = image.Point{r.Min.X + int(int8(info[1])), 0}
+	advance = int(info[0])
+	return
+}
