9c63b6775294640d4c0364bb2086320c74277fd6
Michał Derkacz <michal@Lnet.pl>
2021-08-16 12:06:03 +0200

    pixdisp/subfont/font9: introduce 9font.go utility

---
+++ pixdisp/subfont/font9/9font.go
@@ -0,0 +1,17 @@
+// Copyright 2021 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+// 9font.go translates Plan 9 bitmap font to the Go source.
+//
+// Usage: go run 9font.go FONT_DIR
+package main
+
+import "9fans.net/go/draw"
+
+func main() {
+
+
+}
