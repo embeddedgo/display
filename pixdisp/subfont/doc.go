da9fae511bdf656fa4e9718c34013c44452385d6
Michał Derkacz <michal@Lnet.pl>
2021-08-16 19:21:51 +0200

    subfont: change docstring

--- pixdisp/subfont/doc.go
+++ pixdisp/subfont/doc.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package subfont assumes that a font is a collection of subfonts. A Subfont
+// Package subfont defines the font as a collection of subfonts. A Subfont
 // holds the character data for a specific size, style, weight and specific
 // range of unicode codepoints.
 package subfont
