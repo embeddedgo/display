e0f30666aa26b9a510658db5ad1ec4783b98b2e2
Michał Derkacz <michal@Lnet.pl>
2022-01-11 15:25:37 +0100

    math2d: improve documentation

--- math2d/fixedpoint.go
+++ math2d/fixedpoint.go
@@ -13,37 +13,36 @@ import (
 // convert an integer vector to a fixed-point one. If you want to ensure a
 // specific number of significant bits use A instead.
 func F(v image.Point, n int) image.Point {
-	return image.Point{v.X << n, v.Y << n}
+	return image.Pt(v.X<<n, v.Y<<n)
 }
 
 // I provides a convenient way to convert a fixed-point vector to an integer
 // one. It supports positive and negative n so it can be used to invert F and A
 // as below
 //
-//	I(F(v, n), n) == v // if no overflow
-//	I(A(v, n))    == v // if no overflow and no reduction of significant bits
+//	I(F(v, n), n) == v // true if no overflow
+//	I(A(v, n))    == v // true if no overflow and no significant bit reduction
 func I(v image.Point, n int) image.Point {
 	if n >= 0 {
 		m := uint(n)
 		round := 1 << (m - 1)
-		return image.Point{(v.X + round) >> m, (v.Y + round) >> m}
+		return image.Pt((v.X+round)>>m, (v.Y+round)>>m)
 	}
 	m := uint(-n)
-	return image.Point{v.X << m, v.Y << m}
+	return image.Pt(v.X<<m, v.Y<<m)
 }
 
-// A converts an integer vector to a n bit fixed-point one ensuring that all n
-// bits are used by the value stored in X or Y of the returned vector. It
-// returns the converted vector and the length of fractional part (can be
-// negative if the number of significant bits was reduced). Use A to normalize
-// the vector before passing it to the Rotate or Polar function to improve
-// accuracy.
-func A(v image.Point, n int) (image.Point, int) {
-	x := v.X
+// A converts an integer vector to a fixed-point one ensuring that both f.X
+// and f.Y valeus fit in n bits and at last one of f.X or f.Y has exactly n
+// significant bits. A returns the converted vector and the length of its
+// fractional part (can be negative if the number of significant bits was
+// reduced). For zero v it returns (v, n). Use A to normalize the vector before
+// passing it to the Rotate or Polar functions to improve accuracy.
+func A(v image.Point, n int) (f image.Point, m int) {
+	x, y := v.X, v.Y
 	if x < 0 {
 		x = -x
 	}
-	y := v.Y
 	if y < 0 {
 		y = -y
 	}
@@ -52,9 +51,9 @@ func A(v image.Point, n int) (image.Point, int) {
 	}
 	n -= bits.UintSize - bits.LeadingZeros(uint(x))
 	if n >= 0 {
-		return image.Point{v.X << uint(n), v.Y << uint(n)}, n
+		return image.Pt(v.X<<uint(n), v.Y<<uint(n)), n
 	}
-	m := uint(-n)
-	round := 1 << (m - 1)
-	return image.Point{(v.X + round) >> m, (v.Y + round) >> m}, n
+	k := uint(-n)
+	round := 1 << (k - 1)
+	return image.Pt((v.X+round)>>k, (v.Y+round)>>k), n
 }
