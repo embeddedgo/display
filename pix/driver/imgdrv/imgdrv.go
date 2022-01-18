5e60e63ad37f6ef849cf48ea2abfe0eaeb9cfab5
Michał Derkacz <michal@Lnet.pl>
2022-01-18 18:58:13 +0100

    imgdrv: improve description

--- pix/driver/imgdrv/imgdrv.go
+++ pix/driver/imgdrv/imgdrv.go
@@ -2,8 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package imgdrv provides a driver based on Go image and image/draw packages.
-// It allows to use pix package without any real display, mainly for tests.
+// Package imgdrv is an example of a minimal driver based on Go image/draw
+// package. This package is intended mainly for tests. See ../fbdrv for similar
+// but more efficient drivers.
 package imgdrv
 
 import (
@@ -12,31 +13,41 @@ import (
 	"image/draw"
 )
 
+// Driver provides simplest possible implementation of pix.Driver. Its speed is
+// not important.
 type Driver struct {
 	Image draw.Image
 	fill  image.Uniform
 }
 
+// New returns new Driver using img as frame memory.
 func New(img draw.Image) *Driver {
 	return &Driver{Image: img}
 }
 
+// SetDir implements pix.Driver.
 func (d *Driver) SetDir(dir int) image.Rectangle {
 	return d.Image.Bounds()
 }
 
+// Draw implements pix.Driver.
 func (d *Driver) Draw(r image.Rectangle, src image.Image, sp image.Point, mask image.Image, mp image.Point, op draw.Op) {
 	draw.DrawMask(d.Image, r, src, sp, mask, mp, op)
 }
 
+// SetColor implements pix.Driver.
 func (d *Driver) SetColor(c color.Color) {
 	r, g, b, a := c.RGBA()
 	d.fill.C = color.RGBA64{uint16(r), uint16(g), uint16(b), uint16(a)}
 }
 
+// Fill implements pix.Driver.
 func (d *Driver) Fill(r image.Rectangle) {
 	d.Draw(r, &d.fill, image.Point{}, nil, image.Point{}, draw.Over)
 }
 
-func (d *Driver) Flush()               {}
+// Flush implements pix.Driver.
+func (d *Driver) Flush() {}
+
+// Err implements pix.Driver.
 func (d *Driver) Err(clear bool) error { return nil }
