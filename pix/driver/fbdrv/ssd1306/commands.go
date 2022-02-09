a56ab4527c5a3be179eb74561a3b7daecda282df
Michał Derkacz <michal@Lnet.pl>
2022-02-09 01:14:37 +0100

    ssd1306: commands

--- pix/driver/fbdrv/ssd1306/commands.go
+++ pix/driver/fbdrv/ssd1306/commands.go
@@ -1,20 +1,36 @@
-// Copyright 2021 The Embedded Go Authors. All rights reserved.
+// Copyright 2022 The Embedded Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package ili9341
+package ssd1306
 
 const (
+	LCSA   = 0x00 // Lower column start address for page addressing mode
+	HCSA   = 0x10 // Higher column start address for page addressing mode
+	MAM    = 0x20 // Memory addressing mode
+	CA     = 0x21 // Column start and end address
+	PA     = 0x22 // Page start and end address
 	CSR    = 0x26 // Continuous right horizontal scroll
 	CSL    = 0x27 // Continuous left horizontal scroll
 	CSVR   = 0x29 // Continuous vertical and right horizontal scroll
 	CSVL   = 0x2A // Continuous vertical and left horizontal scroll
+	DSL    = 0x40 // Display start line
 	CONTR  = 0x81 // Set contrast control
-	DISOUT = 0xA4 // Output follows RAM content
-	DISWHT = 0xA5 // Set entire display white
-	DISNOR = 0xA6 // epson.DISNOR // Set normal display
-	DISINV = 0xA7 // epson.DISINV // Set inverse display
-	SLPIN  = 0xAE // = epson.DISOFF // Set sleep mode ON
-	SLPOUT = 0xAF // epson.DISON  // Set sleep mode OFF
-
+	SRM    = 0xA0 // Segment re-map
+	VSA    = 0xA3 // Vertical Scroll Area
+	DOUT   = 0xA4 // Output follows RAM content
+	DWHT   = 0xA5 // Set entire display white
+	DNOR   = 0xA6 // Set normal display
+	DINV   = 0xA7 // Set inverse display
+	MR     = 0xA8 // Multiplex ratio
+	SLPIN  = 0xAE // Set sleep mode ON
+	SLPOUT = 0xAF // Set sleep mode OFF
+	PSA    = 0xB0 // Page start address (0..7) for page addressing mode
+	COMSD  = 0xC0 // COM output scan direction
+	VDO    = 0xD3 // Vertical display offset (0..63)
+	CLKDIV = 0xD5 // Display clock divide ratio / Oscillator frequency
+	PRE    = 0xD9 // Pre-charge period
+	COMPHC = 0xDA // COM pins hardware configuration
+	VCOMH  = 0xDB // VCOMH deselect level
+	NOP    = 0xE3 // No operation
 )
