20498061a0620fb13c1a4af3ce0672aea7e3a800
Michał Derkacz <michal@Lnet.pl>
2022-06-23 20:14:35 +0200

    display/testdata: expose files as embed string variables

---
+++ testdata/embed.go
@@ -0,0 +1,16 @@
+// Copyright 2022 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package testdata
+
+import _ "embed"
+
+//go:embed gopher.png
+var GopherPNG string
+
+//go:embed gopherbug.jpg
+var GopherbugJPG string
+
+//go:embed gopherbug.211x251s27b1
+var Gopherbug211x251s27b1 string
